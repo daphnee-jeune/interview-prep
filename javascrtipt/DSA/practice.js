eec9c560518e38c2fa68522cb87a750a7d59134e
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-03-19 14:49:07 -0500

    lru cache

--- javascrtipt/DSA/practice.js
+++ javascrtipt/DSA/practice.js
@@ -121,6 +121,63 @@ class LRUCache {
     this.cache[key] = value;
   }
 }
+class LRUCacheWithMap {
+  constructor(capacity){
+    this.capacity = capacity
+    this.cache = new Map()
+  }
+  get(key){
+    if(!this.cache.has(key)) return -1
+
+    const value = this.cache.get(key)
+    this.cache.delete(key)
+    this.cache.set(key, value)
+
+    return value
+  }
+  put(key, value){
+    if(this.cache.has(key)){
+      this.cache.delete(key)
+    } else if(this.cache.size >= this.capacity){
+      const lruKey = this.cache.keys().next().value
+      this.cache.delete(lruKey)
+    }
+    this.cache.set(key, value)
+  }
+}
+class LRUwithTTL {
+  constructor(capacity, ttl){
+    this.capacity = capacity;
+    this. ttl = ttl;
+    this.cache = new Map()
+  }
+  _isExpired(key){
+    const entry = this.cache.get(key)
+    if(!entry) return true
+    return Date.now() > entry.expiry
+  }
+
+  get(key){
+    if(!this.cache.has(key) || this._isExpired(key)) {
+      this.cache.delete(key)
+      return null
+    }
+    const { value } = this.cache.get(key)
+    this.cache.delete(key)
+    this.cache.set(key, { value, expiry: Date.now() + this.ttl })
+    return value
+  }
+  set(key, value){
+    if(this.cache.has(key)){
+      this.cache.delete(key)
+    }
+    if(this.cache.size >= this.capacity){
+      const oldestKey = this.cache.keys().next().value
+      this.cache.delete(oldestKey)
+    }
+    this.cache.set(key, { value, expiry: Date.now() + this.ttl })
+  }
+}
 class thing {
   constructor() {
     this.properties = {};
@@ -197,15 +254,15 @@ const otherSubarrayIterative = (nums, k) => {
   let sum = 0; // cumulative sum
   let prefixSum = { 0: 1 }; // store prefix sums and their frequencies
 
-  for(const num of nums){
-    sum += num // add current # to the running sum
+  for (const num of nums) {
+    sum += num; // add current # to the running sum
 
-    if(prefixSum[sum - k] !== undefined){
-      count += prefixSum[sum - k] // add count of subarrays ending at this index that sum to k
+    if (prefixSum[sum - k] !== undefined) {
+      count += prefixSum[sum - k]; // add count of subarrays ending at this index that sum to k
     }
-    prefixSum[sum] = (prefixSum[sum] ?? 0) + 1 // increment freq of current prefix sum
+    prefixSum[sum] = (prefixSum[sum] ?? 0) + 1; // increment freq of current prefix sum
   }
-  return count // total # of subarrays
+  return count; // total # of subarrays
 };
 
 // Two sum
