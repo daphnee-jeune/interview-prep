6b21e44f844a1ee9a09409e3119b5cd3d74d5fdb
Daphnee Jeune <daphneejeune.dev@gmail.com>
2025-05-29 16:32:44 -0500

    work on create user mutation logic

--- react/graphql-project/client/src/App.jsx
+++ react/graphql-project/client/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 import { useQuery, useMutation, gql } from "@apollo/client";
 
 const GET_USERS = gql`
@@ -22,7 +23,17 @@ const GET_USER_BY_ID = gql`
     }
   }
 `;
+
+const CREATE_USER = gql`
+  mutation CreateUser($name: String!, $age: Int!, $isMarried: Boolean!) {
+    createUser(name: $name, age: $age, isMarried: $isMarried) {
+      name
+    }
+  }
+`;
+
 function App() {
+  const [newUser, setNewUser] = useState({});
   const {
     data: usersData,
     loading: isUsersDataLoading,
@@ -35,13 +46,41 @@ function App() {
   } = useQuery(GET_USER_BY_ID, {
     variables: { id: "1" },
   });
+  const [ createUser ] = useMutation(CREATE_USER);
 
   if (isUsersDataLoading || isUserByIdLoading) return <p>Loading...</p>;
   if (usersDataError || userByIdError)
     return <p>Opps an error occured: {usersDataError.message}</p>;
 
+  const handleCreateUser = async () => {
+    console.log(newUser);
+    createUser({
+      variables: {
+        name: newUser.name,
+        age: Number(newUser.age),
+        isMarried: false,
+      },
+    });
+  };
   return (
     <>
+      <div>
+        <input
+          type="text"
+          placeholder="Name..."
+          onChange={(e) =>
+            setNewUser((prev) => ({ ...prev, name: e.target.value }))
+          }
+        />
+        <input
+          type="number"
+          placeholder="Age.."
+          onChange={(e) =>
+            setNewUser((prev) => ({ ...prev, age: e.target.value }))
+          }
+        />
+        <button onClick={handleCreateUser}>Create user</button>
+      </div>
       <div>
         <h1>Chosen User</h1>
         <p>Name: {userById.getUserById.name}</p>
@@ -50,7 +89,7 @@ function App() {
       <div>
         <h1>Users</h1>
         {usersData.getUsers.map((user) => (
-          <div>
+          <div key={user.id}>
             <p>Name: {user.name}</p>
             <p>Age: {user.age}</p>
             <p>Marital status: {user.isMarried ? "Married" : "Single"}</p>
